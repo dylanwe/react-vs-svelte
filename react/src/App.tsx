5e9d85efc23f107e877838359260d833801d5dce
Dylan Weijgertze <dylanwprivate@gmail.com>
2023-11-05 14:08:58 +0100

    removed boilerplate

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.tsx";
-import Detail from "./pages/Detail.tsx";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path={"/"} element={<Home />} />
-                <Route path={"/detail"} element={<Detail />} />
             </Routes>
         </BrowserRouter>
     )
